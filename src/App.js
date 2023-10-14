30947540ba224687d355693df25505acc135cc2c
Your Name~ <you@example.com>
2023-10-14 20:15:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { SketchPicker } from "react-color";
+import "./style.css";
+export default function App() {
+  const [color, setColor] = useState("lightblue");
+  const [hidden, setHidden] = useState(false);
+  const pickerStyle = {
+    default: {
+      picker: {
+        position: "absolute",
+        bottom: "30px",
+        left: "100px"
+      }
+    }
+  };
+  return (
+    <div className="App" style={{ background: color }}>
+      <div className="container">
+        {hidden && (
+          <SketchPicker
+            styles={pickerStyle}
+            color={color}
+            onChange={(updatedColor) => setColor(updatedColor.hex)}
+          />
+        )}
+
+        <button onClick={() => setHidden(!hidden)}>
+          {hidden ? "Close Color Picker" : "Open Color Picker"}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+
